ad5b62261b03124cf55d426babf2a0b70b8c841a
Eternallove7 <614056287@qq.com>
2022-04-20 17:52:25 +0800

    day01

---
+++ pro07-javaweb-begin/src/com/study/servlets/AddServlet.java
@@ -0,0 +1,29 @@
+package com.study.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author RenAshbell
+ * @create 2022-04-20-16:43
+ */
+public class AddServlet extends HttpServlet {
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String fname = request.getParameter("fname");
+        String priceStr = request.getParameter("price");
+        int price = Integer.parseInt(priceStr);
+        String fcountStr = request.getParameter("fcount");
+        int fcount = Integer.parseInt(fcountStr);
+        String remark = request.getParameter("remark");
+
+        System.out.println("fname = " + fname);
+        System.out.println("price = " + price);
+        System.out.println("fcount = " + fcount);
+        System.out.println("remark = " + remark);
+
+    }
+}
