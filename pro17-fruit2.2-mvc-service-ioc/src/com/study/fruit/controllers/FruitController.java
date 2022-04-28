98731e63343ae99a357061d4ec59f68a49f2f256
Eternallove7 <614056287@qq.com>
2022-04-28 17:30:57 +0800

    day07

---
+++ pro17-fruit2.2-mvc-service-ioc/src/com/study/fruit/controllers/FruitController.java
@@ -0,0 +1,102 @@
+package com.study.fruit.controllers;
+
+import com.study.fruit.biz.FruitService;
+import com.study.fruit.biz.impl.FruitServiceImpl;
+import com.study.fruit.pojo.Fruit;
+import com.study.myssm.util.StringUtil;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
+public class FruitController {
+    private FruitService fruitService = new FruitServiceImpl();
+
+    private String update(Integer fid,String fname,Integer price,Integer fcount,String remark){
+        //3.执行更新
+        fruitService.updateFruit(new Fruit(fid,fname, price ,fcount ,remark ));
+
+        //4.资源跳转
+        //super.processTemplate("index",request,response);
+        //request.getRequestDispatcher("index.html").forward(request,response);
+        //此处需要重定向，目的是重新给IndexServlet发请求，重新获取furitList，然后覆盖到session中，这样index.html页面上显示的session中的数据才是最新的
+        return "redirect:fruit.do";
+    }
+
+    private String edit(Integer fid,HttpServletRequest request) {
+        if(fid != null){
+            Fruit fruit = fruitService.getFruitByFid(fid);
+            request.setAttribute("fruit",fruit);
+//            super.processTemplate("edit",request,response);
+            return "edit";
+        }
+        return "error";
+    }
+
+    private String del(Integer fid) {
+        if(fid != null){
+            fruitService.delFruit(fid);
+
+            //super.processTemplate("index",request,response);
+//            response.sendRedirect("fruit.do");
+            return "redirect:fruit.do";
+        }
+        return "error";
+    }
+
+    private String add(String fname,Integer price,Integer fcount,String remark) {
+        Fruit fruit = new Fruit(0,fname , price , fcount , remark ) ;
+        fruitService.addFruit(fruit);
+//        response.sendRedirect("fruit.do");
+        return "redirect:fruit.do";
+
+    }
+
+    private String index(String oper,String keyword,Integer pageNo,HttpServletRequest request) {
+        HttpSession session = request.getSession() ;
+        if (pageNo == null){
+            pageNo = 1 ;
+        }
+        //如果oper!=null 说明 通过表单的查询按钮点击过来的
+        //如果oper是空的，说明 不是通过表单的查询按钮点击过来的
+        if(StringUtil.isNotEmpty(oper) && "search".equals(oper)){
+            //说明是点击表单查询发送过来的请求
+            //此时，pageNo应该还原为1 ， keyword应该从请求参数中获取
+            pageNo = 1 ;
+            //如果keyword为null，需要设置为空字符串""，否则查询时会拼接成 %null% , 我们期望的是 %%
+            if(StringUtil.isEmpty(keyword)){
+                keyword = "" ;
+            }
+            //将keyword保存（覆盖）到session中
+            session.setAttribute("keyword",keyword);
+        }else{
+            //说明此处不是点击表单查询发送过来的请求（比如点击下面的上一页下一页或者直接在地址栏输入网址）
+            //此时keyword应该从session作用域获取
+            //如果不是点击的查询按钮，那么查询是基于session中保存的现有keyword进行查询
+            Object keywordObj = session.getAttribute("keyword");
+            if(keywordObj!=null){
+                keyword = (String)keywordObj ;
+            }else{
+                keyword = "" ;
+            }
+        }
+
+        // 重新更新当前页的值
+        session.setAttribute("pageNo",pageNo);
+
+        List<Fruit> fruitList = fruitService.getFruitList(keyword , pageNo);
+        session.setAttribute("fruitList",fruitList);
+
+        //总记录条数
+        int pageCount = fruitService.getPageCount(keyword);
+        session.setAttribute("pageCount",pageCount);
+
+        //此处的视图名称是 index
+        //那么thymeleaf会将这个 逻辑视图名称 对应到 物理视图 名称上去
+        //逻辑视图名称 ：   index
+        //物理视图名称 ：   view-prefix + 逻辑视图名称 + view-suffix
+        //所以真实的视图名称是：      /       index       .html
+//        super.processTemplate("index",request,response);
+        return "index";
+    }
+}
---
+++ pro18-servlet-api/readme.txt
@@ -0,0 +1,50 @@
+1.再次学习servlet的初始化方法
+    1).Servlet生命周期：实例化、初始化、服务、销毁
+    2).Servlet中的初始化方法有两个：init() , init(config)
+       其中带参数的方法代码如下:
+       public void init(ServletConfig config){
+        this.config = config;
+        init();
+       }
+       另一个无参的init方法如下:
+       public void init() throws ServletException{
+
+       }
+       如果我们想要在Servlet初始化做一些准备工作，我们就要重写init
+       我们可以通过如下步骤去获取初始化设置的数据
+       - 获取config对象 : ServletConfig config = getServletConfig();
+       - 获取初始化参数值 : config.getInitParameter(key);
+    3) 在web.xml文件中配置servlet
+
+    4) 也可以通过注解的方式进行配置
+
+2.学习Servlet中的ServletContext和<context-param>
+    1) 获取ServletContext, 有很多方法
+        在初始化方法中 : ServletContext ServletContext = getServletContext();
+        在服务方法中也可以通过request对象获取 , 也可以通过session获取
+        req.getServletContext(); req.getSession().getServletContext();
+    2) 获取初始化值:
+        servletContext.getInitParameter();
+
+3.什么是业务层
+    1) Model1和Model2
+    MVC : Model(模型)、View(视图)、Controller(控制器)
+    视图层 : 用于做数据展示以及和用户交互的一个界面
+    控制层 : 能够接受客户端的请求,具体的业务功能还是要借助模型组件来完成
+    模型层 : 模型分为很多种 : 有比较简单的pojo/vo(value object) , 有业务模型组件 , 有数据访问层组价
+        1) pojo/vo: 值对象
+        2) DAO: 数据访问对象
+        3) BO: 业务对象
+
+    区分业务对象和数据访问对象 :
+    1) DAO中的方法都是单精度方法 一个方法只考虑一个操作 , 比如添加就是insert操作、查询就是select操作
+    2) BO中的方法属于业务方法，也实际的业务是比较复杂的，因此业务方法的粒度还是比较粗的
+    注册这个功能属于业务功能，也就是说注册这个方法属于业务方法
+    那么这个业务方法中包含了多个DAO方法。也就是说注册这个业务功能需要通过多个DAO方法的组合调用，从而完成注册功能的开发
+    注册：
+        1. 检查用户名是否已被注册 - DAO中的查询操作
+        2. 向用户表新增一条新用户记录 - DAO中的insert操作
+        3. 向用户积分表中新增一条记录 - DAO中的insert操作
+        4. 向系统消息表新增一条记录 - DAO中的insert操作
+        5. 向系统日志表新增一条记录(某用户在某时某分) - DAO中的insert操作
+        6. ...
